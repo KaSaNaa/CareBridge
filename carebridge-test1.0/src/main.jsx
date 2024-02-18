6b3ea226bb293d45b084b6b01a777be53ba29af3
KaSaNaa <pasindudeshitha123@gmail.com>
2024-02-19 01:06:23 +0530

    Sign-Up UI DONE!

--- carebridge-test1.0/src/main.jsx
+++ carebridge-test1.0/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import App from './App.jsx'
+import App from './components/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
