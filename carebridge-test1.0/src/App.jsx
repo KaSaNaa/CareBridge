6bb6b8afc34faae02356e5ec8c2163bbe0255302
KaSaNaa <pasindudeshitha123@gmail.com>
2024-02-25 01:39:11 +0530

    started designing doctor's UIs

--- carebridge-test1.0/src/App.jsx
+++ carebridge-test1.0/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Auth from './pages/Auth/Auth.jsx'; // Add missing import statement for Auth component
-import DoctorDashboard from './pages/Doctor/DoctorDashboard.jsx';
+import DoctorDashboard from './pages/Doctor/DashboardPage.jsx';
 
 const App = () => {
   return (
